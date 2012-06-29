b18dccfaa3d7c4638b34ca690982f1cd630a78f3
Nailton Gomes <nailtongomes@ig.com.br>
2012-06-29 17:30:52 -0300

    rating heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Arg::Application.initialize!
-
-config.gem "ajaxful_rating"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,8 @@ Arg::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.gem "ajaxful_rating"
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
