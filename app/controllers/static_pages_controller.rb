2e83faa64fdf0b7cb9cddf9f73d8d0aaa5ce54f7
Nailton Gomes <nailtongomes@ig.com.br>
2012-07-17 22:33:32 -0300

    bug pg

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -17,13 +17,14 @@ class StaticPagesController < ApplicationController
   end
 
   def top
-    #hash com os melhores argumentos pelos usuários
-    @top_c = Rate.average(:stars, :conditions => "rateable_type = 'Argument' AND dimension = 'comunidade'", :group => :rateable_id, :order => 'stars DESC, rateable_id')
+    #hash com os melhores argumentos pelos usuários  
+   
+    @top_c = Rate.average(:stars, :conditions => "rateable_type = 'Argument' AND dimension = 'comunidade'", :group => [:rateable_id, :stars], :order => 'stars DESC, rateable_id')
     @top_c.delete_if {|k, v| v < 4 }
     @top_c = Hash[@top_c.sort_by { |k, v| v }.reverse!]
 
     #hash com os melhores argumentos pelos moderadores
-    @top_m = Rate.average(:stars, :conditions => "rateable_type = 'Argument' AND dimension = 'moderador'", :group => :rateable_id, :order => 'stars DESC, rateable_id')
+    @top_m = Rate.average(:stars, :conditions => "rateable_type = 'Argument' AND dimension = 'moderador'", :group => [:rateable_id, :stars], :order => 'stars DESC, rateable_id')
     @top_m.delete_if {|k, v| v < 4 }
     @top_m = Hash[@top_m.sort_by { |k, v| v }.reverse!]
   end
