c9d55f9d769a6679c4e82c51086f9d67018ada8c
Nailton Gomes <nailtongomes@ig.com.br>
2012-07-22 18:42:28 -0300

    Mailer

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,3 @@
-ActionMailer::Base.smtp_settings = {
-  :user_name => "app5625600@heroku.com",
-  :password => "carcara1he",
-  :domain => "argumentos.herokuapp.com",
-  :address => "smtp.sendgrid.net",
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
-
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,20 +64,20 @@ Arg::Application.configure do
   
   config.gem "ajaxful_rating"
   
-#  config.action_mailer.default_url_options = { :host => 'argumentos.herokuapp.com' }
-#  config.action_mailer.delivery_method = :smtp
-#  config.action_mailer.perform_deliveries = true
-#  config.action_mailer.raise_delivery_errors = false
-#  config.action_mailer.default :charset => "utf-8"
-#  config.action_mailer.smtp_settings = {
-#    address: "smtp.gmail.com",
-#    port: 587,
-#    domain: "argumentos.herokuapp.com",
-#    authentication: "plain",
-#    enable_starttls_auto: true,
-#    user_name: ENV["GMAIL_USERNAME"],
-#    password: ENV["GMAIL_PASSWORD"]
-#  }
+  config.action_mailer.default_url_options = { :host => 'argumentos.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "argumentos.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
