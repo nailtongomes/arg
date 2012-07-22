5a7396f6956e51cf302365dc3c7b3727349e93b5
Nailton Gomes <nailtongomes@ig.com.br>
2012-07-22 17:12:37 -0300

    Mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,16 @@
+class UserMailer < ActionMailer::Base
+  #default to: "nailton.arg@gmail.com"
+
+  def signup_notification(user)
+    recipients "#{user.name} <#{user.email}>"
+    from       "nailton.arg@gmail.com"
+    subject    "Seu usuário #{user.name} foi criado."
+    sent_on    Time.now
+  end
+
+  #def notification(inquiry)
+  #  @inquiry = inquiry
+  #  mail :reply_to => @inquiry.email, :subject => "New inquiry"
+  #end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -43,6 +43,8 @@ class User < ActiveRecord::Base
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
 
+  after_create :deliver_signup_notification  
+
   validates :name, presence: true, length: { maximum: 40 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
@@ -73,5 +75,11 @@ class User < ActiveRecord::Base
 
   def create_remember_token
     self.remember_token = SecureRandom.urlsafe_base64
+  end
+  
+  protected
+
+  def deliver_signup_notification
+    UserMailer.deliver_signup_notification(self)
   end
 end
